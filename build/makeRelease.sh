4d774091b34c1e7a88fac879f8af17b6467df809
Anna Battenhouse <abattenhouse@utexas.edu>
2014-09-05 17:17:51 -0500

    (r24) Tarball build support

--- build/makeRelease.sh
+++ build/makeRelease.sh
@@ -2,17 +2,37 @@
 
 RELVER=$1
 
+err() {
+  echo "** ERROR: $1...exiting";
+  if [ "$no_exit" == "" ]; then exit 1; fi
+}
+
+echo "---------------------------------------------";
+echo "makeRelease '$RELVER' - `date`";
+if [ "$RELVER" == "" ]; then err "No release version provided"; fi
+
+echo "..Checking mirUtils version for string '$RELVER'...";
+CMD="grep -P '$RELVER' ../mirUtils";
+STR="`$CMD`";
+if [ "$STR" == "" ]; then err "Release version '$RELVER' not found in ../mirUtils"; fi
+
 TMP_VDIR="mirUtils-$RELVER";
+echo "..Preparing release directory '$TMP_VDIR'...";
 if [   -d "$TMP_VDIR" ]; then rm -rf "$TMP_VDIR"; fi
-if [ ! -d "$TMP_VDIR" ]; then mkdir "$TMP_VDIR"; fi
+if [ ! -d "$TMP_VDIR" ]; then mkdir -p "$TMP_VDIR/test/data"; fi
 
-cp  -p ../COPYING  $TMP_VDIR/
-cp  -p ../README   $TMP_VDIR/
-cp  -p ../mirUtils $TMP_VDIR/
-cp -rp ../lib      $TMP_VDIR/lib
-cp -rp ../mirbase  $TMP_VDIR/mirbase
+cp  -p ../COPYING           $TMP_VDIR/
+cp  -p ../README            $TMP_VDIR/
+cp  -p ../test/data/*small* $TMP_VDIR/test/data
+cp  -p ../mirUtils          $TMP_VDIR/
+cp -rp ../lib               $TMP_VDIR/lib
+cp -rp ../mirbase           $TMP_VDIR/mirbase
 
+echo "..Preparing $TMP_VDIR.tar.gz...";
 tar -cf "$TMP_VDIR.tar" $TMP_VDIR
 gzip "$TMP_VDIR.tar"
+mv "$TMP_VDIR.tar.gz" versions/
 
 rm -rf "$TMP_VDIR"
+
+echo "..Done! - `date`";
